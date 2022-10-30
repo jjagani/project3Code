5a59ecb546eb03638ee7549afa10d729d24a9795
jjagani <jjagani@purdue.edu>
2022-10-30 13:15:14 -0400

    changes made

--- robotCode.py
+++ robotCode.py
@@ -3,6 +3,9 @@ import grovepi
 import time
 import brickpi3 
 
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division 
+
 #Notes:
 #Right side line following  (probably will change)
 
@@ -10,11 +13,7 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 #Sensor & Motor Ports
 driveLeft = BP.PORT_A
 driveRight = BP.PORT_B
-
-#Error Bounds
-line_bound = 2
-#Control Gains
-p = 0.8
+color_sensor = BP.PORT_1
 
 def driveForward(speed):
     BP.set_motor_power(driveLeft + driveRight,  speed)
@@ -35,12 +34,12 @@ def arcTurnLeft(speed):
 def arcTurnRight(speed):
     BP.set_motor_power(driveLeft, speed)
     BP.set_motor_power(driveRight, p * speed)
-
+#Determine whether accuracy is in acceptable range
 def inRange(value, targetValue, bound):
     if(value < (targetValue + bound) and value > (targetValue - bound)):
         return 1
     return 0
-
+#Line following path correction
 def correctPath(lightValue, targetValue, speed):
     if(inRange(lightValue, targetValue, line_bound)):
         pass
@@ -51,9 +50,27 @@ def correctPath(lightValue, targetValue, speed):
         while(not(inRange(lightValue, targetValue, line_bound))):
             arcTurnRight(speed)
 
+def addColorSensor(port):
+    BP.set_sensor_type(port, BP.SENSOR_TYPE.EV3_INFRARED_PROXIMITY)
 
+
+def readColorSensor():
+    BP.get_sensor retrieves a sensor value.
+
+#==========
+#MAIN LOOP
+#==========
+
+
+
+#Error Bounds
+line_bound = 2
+#Control Gains
+p = 0.8 
+
+try:
+    while True:
         
-
 
 
 
